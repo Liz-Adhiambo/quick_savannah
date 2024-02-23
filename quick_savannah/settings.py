ac037c01394fbca89fa50b8d535e80a49b64ca71
Liz-Adhiambo <adhiamboliz3@gmail.com>
2024-02-23 20:27:02 +0300

    Added trigger notifications

--- quick_savannah/settings.py
+++ quick_savannah/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("securitykey", default='test')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-ca6f.up.railway.app','127.0.0.1']
 
 
 # Application definition
